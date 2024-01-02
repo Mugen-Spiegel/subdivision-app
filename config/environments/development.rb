f8575dc9e28cf535f9158cdf3f1d483f1439aaf7
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-02 19:08:06 +0800

    update the development url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts  << "subdivision-tool-development.up.railway.app"
+  config.hosts  << "subdivision-app-development.up.railway.app"
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
