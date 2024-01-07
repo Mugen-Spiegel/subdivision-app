4d934e88fe338a9c91cdaae673aac3c4ba513958
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-07 22:08:34 +0800

    add cancan rescue 403.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 
+    rescue_from CanCan::AccessDenied do |exception|
+        render :file => 'public/403.html', :status => :forbidden, :layout => false
+      end
+    
 
 
     private
