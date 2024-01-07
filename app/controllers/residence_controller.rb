e0499b962ad5a8792934493e2e110be9e3156d60
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-07 22:09:16 +0800

    return sign_in_url

--- app/controllers/residence_controller.rb
+++ app/controllers/residence_controller.rb
@@ -3,5 +3,7 @@ class ResidenceController < ApplicationController
     def index
         residence_repositories = ResidenceRepositories.new(params, subdivision)
         @residence =  residence_repositories.search_residence
+        complete_url = request.original_url
+        @sign_in_url = "#{complete_url.gsub("residence","")}subdivision_setting/" + subdivision.uuid + "/prepaire_register_link"
     end
 end
