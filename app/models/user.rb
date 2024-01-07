e2bb6603610a4aa337b741cefc060dfac59a16aa
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-07 22:14:32 +0800

    remove optional in subdivisioin association, add admin false

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
-    belongs_to :subdivision, optional: true
+    belongs_to :subdivision
     has_many :water_billing_transactions
     has_many :monthly_due_transaction
     validates :first_name, :middle_name, :last_name, :block, :lot, :street, presence: true
@@ -12,11 +12,11 @@ class User < ApplicationRecord
         if month > 1
             month -= 1
         else
-            year = nil
-            month = nil
+            year = Time.now.year.to_i - 1       
+            month = 12
         end
         joins(:water_billing_transactions)
-        .where("water_billing_transactions.year": year, "water_billing_transactions.month": month)
+        .where("water_billing_transactions.year": year, "water_billing_transactions.month": month, "users.admin": false)
         .select("water_billing_transactions.id AS transaction_id,
             users.id AS user_id, 
             water_billing_transactions.year as year,
@@ -25,5 +25,7 @@ class User < ApplicationRecord
             water_billing_transactions.current_reading AS current_reading")
     end
 
-    scope :all_new_user, -> { left_joins(:water_billing_transactions).where("water_billing_transactions.user_id IS ?", nil)  }
+    scope :all_new_user, -> { 
+        left_joins(:water_billing_transactions).where("users.admin=false AND water_billing_transactions.user_id IS ?", nil)  
+    }
 end
