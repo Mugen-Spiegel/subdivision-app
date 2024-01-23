98512982c63207c612f587223aa1f2b0b4a286d6
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-23 20:55:09 +0800

    reword the method

--- app/controllers/subdivision_setting_controller.rb
+++ app/controllers/subdivision_setting_controller.rb
@@ -1,8 +1,8 @@
 class SubdivisionSettingController < ApplicationController
     
-    skip_before_action :authenticate_user!, only: [:prepaire_register_link]
+    skip_before_action :authenticate_user!, only: [:prepare_register_link]
 
-    def prepaire_register_link
+    def prepare_register_link
         complete_url = request.original_url.split("subdivision_setting")
         unless Subdivision.find_by(uuid: params["id"]).nil?
             redirect_to "/users/sign_up?subdivision_uuid=#{params["id"]}"
