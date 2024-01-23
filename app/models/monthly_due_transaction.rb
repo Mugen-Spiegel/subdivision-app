11b51925081b8d8e72f538cebb3d7579aedeb689
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-23 21:20:49 +0800

    remove status, and rename field

--- app/models/monthly_due_transaction.rb
+++ app/models/monthly_due_transaction.rb
@@ -1,14 +1,10 @@
 class MonthlyDueTransaction < ApplicationRecord
 
+    include StatusList
+
     belongs_to :user
     belongs_to :subdivision
-    has_one :monthly_due_transaction, through: :user
     
-    PARTIAL = "partial"
-    PAID = "paid"
-    UN_PAID = "unpaid"
-    STATUS = [UN_PAID, PAID, PARTIAL]
-
-    validates :is_paid, :amount, :user_id, :year, :month, :monthly_due_id, presence: true
+    validates :is_paid, :bill_amount, :user_id, :year, :month, :monthly_due_id, presence: true
 
 end
