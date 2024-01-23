a92b6198dffe952a2461f9114399f32abfd0864e
Jophat Tamayo <tamay06131993@gmail.com>
2024-01-23 20:54:29 +0800

    for the profile of invidual

--- app/controllers/residence_controller.rb
+++ app/controllers/residence_controller.rb
@@ -1,9 +1,22 @@
 class ResidenceController < ApplicationController
 
+
+    before_action :residence, only: %i[ show ]
+
     def index
         residence_repositories = ResidenceRepositories.new(params, subdivision)
         @residence =  residence_repositories.search_residence
         complete_url = request.original_url
-        @sign_in_url = "#{complete_url.gsub("residence","")}subdivision_setting/" + subdivision.uuid + "/prepaire_register_link"
+        @sign_in_url = "#{complete_url.gsub("residence","")}subdivision_setting/" + subdivision.uuid + "/prepare_register_link"
+    end
+
+    def show
+
+    end
+
+    private
+
+    def residence
+        @residence = User.find_by(uuid: params[:id])
     end
 end
